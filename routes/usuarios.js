ffe54716139b35855b1ad3599d95e4f732e67e5b
MrLuXuS <gggggsaucedo@gmail.com>
2025-06-16 20:26:30 -0400

    test momentp

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 router.post('/crearUsuario', usuarioController.crearUsuarioController);
 router.patch('/editarUsuario', usuarioController.editarUsuarioController);
-router.post('/login', usuarioController.loginUsuarioController);
+router.post('/login', (req, res) => {
+  res.send('LOGIN FUNCIONANDO');
+});
 router.post('/usuarios', usuarioController.obtenerUsuariosController);
 router.post('/verificar-codigo', usuarioController.verificarCodigoController);
 module.exports = router;
